dae6e9b853555ca2b026ef4d6b56af9162175a86
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-08-14 18:32:51 +0900

    TypeScript導入

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/vue',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: [
+    'prettier',
+    '@typescript-eslint'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-unused-vars': 'off'
+  }
+}
---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['js', 'vue', 'ts', 'json'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ]
+}
